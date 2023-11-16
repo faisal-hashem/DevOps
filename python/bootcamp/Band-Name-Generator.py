0bb0a9d84a1bd01eb6f2fc85a459c9e51175c866
Faisal Hashem <fhashem@generalatlantic.com>
2023-11-15 20:10:15 -0500

    Updated

---
+++ python/bootcamp/Band-Name-Generator.py
@@ -0,0 +1,8 @@
+def my_func():
+    print("Welcome to the Band Name Generator")
+    band_name = input("What is the name of the city you grew up in? \n")
+    pet_name = input("What is your pet name? \n")
+    print("Your band name could be", band_name + " " + pet_name)
+
+
+my_func()
