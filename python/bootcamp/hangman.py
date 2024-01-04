c8bc3c12dcc64dc6f662909f2ef20c05b1ab647a
Faisal Hashem <fhashem@generalatlantic.com>
2024-01-03 21:47:53 -0500

    Updated

---
+++ python/bootcamp/hangman.py
@@ -0,0 +1,94 @@
+import random
+hangmanpics = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========''']
+
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+# TODO-1: - Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
+display = []
+for c in range(len(chosen_word)):
+    display += "_"
+
+print(display)
+
+# TODO-2: - Loop through each position in the chosen_word;
+# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+hangman = 0
+while "_" in display:
+    status = 0
+    guess = input("Guess a letter: ").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if guess == letter:
+            display[position] = letter
+            status += 1
+
+    if status == 0:
+        hangman += 1
+
+    if "_" not in display:
+        print("You won!")
+
+    print(hangmanpics[hangman])
+    print(display)
+
+    if hangman == 6:
+        print("You lost")
