2fbf0909dc736a062c47447c0484fd6f109f578c
Faisal Hashem <fhashem@generalatlantic.com>
2024-02-03 12:11:50 -0500

    Updated

--- python/bootcamp/coffee-machine/coffee-machine.py
+++ python/bootcamp/coffee-machine/coffee-machine.py
@@ -8,110 +8,85 @@ money = 0
 
 
 def coins():
+    """Returns total calculated from coins inserted"""
     print("Please insert coins.")
-    quarters = (int(input("how many quarters?: "))) * .25
-    dimes = (int(input("how many dimes?: "))) * .10
-    nickles = (int(input("how many nickles?: "))) * .05
-    pennies = (int(input("how many pennies?: "))) * .01
-    total = quarters + dimes + nickles + pennies
+    total = (int(input("how many quarters?: "))) * .25
+    total += (int(input("how many dimes?: "))) * .10
+    total += (int(input("how many nickles?: "))) * .05
+    total += (int(input("how many pennies?: "))) * .01
     return total
 
 
-# def cashier(choice, money):
-#     if choice == "espresso":
-#         if money >= MENU[choice]['cost']:
-#             change = money - MENU[choice]['cost']
-#             return change
-#         else:
-#             print("Sorry that's not enough money. Money refunded.")
-
-def validate_inventory(choice):
-    # Get the required ingredients for the chosen drink
+def validate(choice, water, milk, coffee):
+    """Validates if there are enough resources and returns a True/False"""
+    my_resources = {
+        "water": water,
+        "milk": milk,
+        "coffee": coffee,
+    }
     stock = MENU[choice]["ingredients"]
+    for i, a in stock.items():
+        if my_resources[i] < a:
+            print(f"Sorry there is not enough {i}")
+            return False
+    return True
 
-    for ingredient, amount_required in stock.items():
-        if resources[ingredient] >= amount_required:
-            continue  # Enough of this ingredient
+
+def menu(choice):
+    """Pulls Menu from Dictionary"""
+    if choice == "espresso":
+        co_water = MENU[choice]['ingredients']['water']
+        co_milk = 0
+        co_coffee = MENU[choice]['ingredients']['coffee']
+        co_cost = MENU[choice]['cost']
+        return co_water, co_milk, co_coffee, co_cost
+    else:
+        co_water = MENU[choice]['ingredients']['water']
+        co_milk = MENU[choice]['ingredients']['milk']
+        co_coffee = MENU[choice]['ingredients']['coffee']
+        co_cost = MENU[choice]['cost']
+        return co_water, co_milk, co_coffee, co_cost
+
+
+def cashier(choice, money):
+    """Processes change after coins are inserted"""
+    money = 0
+    customer_cash = coins()
+    item_cost = MENU[choice]["cost"]
+    if customer_cash >= item_cost:
+        change = round((customer_cash - item_cost), 2)
+        money += item_cost
+        return money, change
+    else:
+        return 0
+
+
+coffee_power_on = True
+while coffee_power_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if choice == "report":
+        print(f"Water: {water}ml")
+        print(f"Milk: {milk}ml")
+        print(f"Coffee: {coffee}g")
+        print(f"Money: ${money}")
+    elif choice == "off":
+        coffee_power_on = False
+    elif choice not in ("espresso", "latte", "cappuccino"):
+        print(f"{choice} is an invalid entry. Please try again.")
+    elif validate(choice, water, milk, coffee):
+        item = menu(choice)
+        transaction = cashier(choice, money)
+
+        if transaction == 0:
+            print("Sorry that's not enough money. Money refunded.")
         else:
-            print(f"Sorry, there is not enough {ingredient}.")
-            return False  # Not enough of this ingredient
+            money = transaction[0]
+            change = transaction[1]
+            print(f"Here is ${change} in change")
+            print(f"Here is your {choice}. Enjoy!")
+            water -= item[0]
+            if "milk" in MENU[choice]['ingredients']:
+                milk -= item[1]
 
-    return True  # Enough of all ingredients
-
-
-choice = "latte"
-if validate_inventory(choice):
-    print(f"Enough resources for {choice}")
-else:
-    print(f"Not enough resources for {choice}")
-
-# def validate_inventory(choice, water, milk, coffee):
-#     resources = [water, milk, coffee]
-#     stock = MENU[choice]["ingredients"]
-#     for i in stock.values():
-#         for r in resources:
-#             if i > r:
-#                 continue
-#             else:
-#                 print("Missing {stock.keys}")
-
-
-# def menu(choice):
-#     if choice == "espresso":
-#         co_water = MENU[choice]['ingredients']['water']
-#         co_coffee = MENU[choice]['ingredients']['coffee']
-#         co_cost = MENU[choice]['cost']
-#         return co_water, co_coffee, co_cost
-#     else:
-#         co_water = MENU[choice]['ingredients']['water']
-#         co_milk = MENU[choice]['ingredients']['milk']
-#         co_coffee = MENU[choice]['ingredients']['coffee']
-#         co_cost = MENU[choice]['cost']
-#         return co_water, co_milk, co_coffee, co_cost
-
-
-# coffee_power_on = True
-# while coffee_power_on:
-#     choice = input("What would you like? (espresso/latte/cappuccino): ")
-
-#     if choice == "report":
-#         print(f"Water: {water}")
-#         print(f"Milk: {milk}")
-#         print(f"Coffee: {coffee}")
-#         print(f"Money: {money}")
-#     elif choice == "off":
-#         coffee_power_on = False
-#     else:
-#         validation = validate_inventory(choice, water, milk, coffee)
-#         if validation == 1:
-#             item = menu(choice)
-#             if choice == "espresso":
-#                 item_cost = item[2]
-#             else:
-#                 item_cost = item[3]
-
-#             if water > item[0]:
-#                 water -= item[0]
-#             else:
-#                 print("Sorry there is not enough water.")
-
-#             if "milk" in MENU[choice]['ingredients']:
-#                 if milk > item[1]:
-#                     milk -= item[1]
-#                 else:
-#                     print("Sorry there is not enough milk.")
-
-#             if coffee > item[2]:
-#                 coffee -= item[2]
-#             else:
-#                 print("Sorry there is not enough coffee.")
-
-#             customer_cash = coins()
-#             if customer_cash >= item_cost:
-#                 change = round((customer_cash - item_cost), 2)
-#                 money += item_cost
-#                 print(f"Here is ${change} in change")
-#                 print(f"Here is your {choice}. Enjoy!")
-
-#             else:
-#                 print("Sorry that's not enough money. Money refunded.")
+            coffee -= item[2]
