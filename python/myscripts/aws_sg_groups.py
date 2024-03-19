4c4fddcb4d5ecc743751ee548bf4fef20493f9c2
Faisal Hashem <faisal-hashem@outlook.com>
2024-03-19 07:50:13 -0400

    updated

--- python/myscripts/aws_sg_groups.py
+++ python/myscripts/aws_sg_groups.py
@@ -1,6 +1,22 @@
+# Scan for all exisitng security groups 
+# in all regions with name, description, 
+# last modified date, inbound rules.
+
 import boto3
 
 ec2 = boto3.resource('ec2')
-sg = ec2.SecurityGroup('group_name')
-for s in sg:
-    print(s)
+ec2_client = boto3.client('ec2')
+
+sg = ec2.security_groups.all()
+response = ec2_client.describe_regions()
+regions = [region['RegionName'] for region in response['Regions']]
+
+for region in regions:
+    print(region)
+
+all_groups = []
+
+for group in sg:
+    all_groups.append(group.group_name)
+
+print(all_groups)
