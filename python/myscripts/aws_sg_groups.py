f2c3ee45f545ca765deb3c942c01f7ca6af5c3df
Faisal Hashem <faisal-hashem@outlook.com>
2024-03-24 00:16:05 -0400

    Updated

--- python/myscripts/aws_sg_groups.py
+++ python/myscripts/aws_sg_groups.py
@@ -3,17 +3,30 @@
 # last modified date, inbound rules.
 
 import boto3
+from botocore.config import Config
 
-ec2 = boto3.resource('ec2')
-ec2_client = boto3.client('ec2')
+def list_all_security_groups(region='us-east-2'):
+    my_config = Config(region_name = region)
+    ec2 = boto3.client("ec2", config=my_config)
+    security_groups = ec2.describe_security_groups()['SecurityGroups'] 
+    security_dict = {}
+    for security_group in security_groups:
+        group_name = security_group['GroupName']
+        sg_id = security_group['GroupId']
+        security_dict[group_name] = sg_id
+    return security_dict
 
-sg = ec2.security_groups.all()
-response = ec2_client.describe_regions()
-regions = [region['RegionName'] for region in response['Regions']]
-all_groups = []
+print(list_all_security_groups())
+# ec2 = boto3.resource('ec2')
+# ec2_client = boto3.client('ec2')
 
-for region in regions:
-    for group in sg:
-        all_groups.append(group.group_name)
+# sg = ec2.security_groups.all()
+# response = ec2_client.describe_regions()
+# regions = [region['RegionName'] for region in response['Regions']]
+# all_groups = []
 
-print(all_groups)
+# for region in regions:
+#     for group in sg:
+#         all_groups.append(group.group_name)
+
+# print(all_groups)
