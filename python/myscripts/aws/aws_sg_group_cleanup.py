e8fe04b1632935884139a16e71751aee11a88736
Faisal Hashem <faisal-hashem@outlook.com>
2024-03-28 18:43:54 -0400

    Added AWS SG Cleanup Script

--- python/myscripts/aws/aws_sg_group_cleanup.py
+++ python/myscripts/aws/aws_sg_group_cleanup.py
@@ -3,6 +3,7 @@
 # last modified date, inbound rules.
 
 import boto3
+import botocore
 from botocore.config import Config
 
 def list_all_security_groups(region='us-east-2'):
@@ -19,23 +20,27 @@ def list_all_security_groups(region='us-east-2'):
 # Function to scan all EC2 instances attached security groups against list of exisitng security groups
 #If the security groups does not match, delete the security group
 
-def report_not_used_security_groups(region):
+def report_not_used_security_groups(region='us-east-2'):
     my_config = Config(region_name=region)
     ec2 = boto3.client("ec2", config=my_config)
     rds = boto3.client("rds", config=my_config)
     elb = boto3.client("elb", config=my_config)
     elbv2 = boto3.client("elbv2", config=my_config)
+    elastic_cache = boto3.client("elasticache", config=my_config)
+    redshift = boto3.client("redshift", config=my_config)
     
     existing_security_groups = list_all_security_groups(region)
     ec2_instances = ec2.describe_instances()['Reservations']
     rds_instances = rds.describe_db_instances()["DBInstances"]
     elb_instances = elb.describe_load_balancers()["LoadBalancerDescriptions"]
     elbv2_instances = elbv2.describe_load_balancers()["LoadBalancers"]
+    elasticcache_clusters = elastic_cache.describe_cache_clusters()["CacheClusters"]
+    redshift_clusters = redshift.describe_clusters()["Clusters"]
     
     instances_sg = []
     security_groups = []
     
-    if ec2_instances[0]["Instances"][0]["SecurityGroups"] != []:
+    if ec2_instances != []:
         for instance in ec2_instances:
             ec2_sg_id = instance["Instances"][0]["SecurityGroups"][0]["GroupId"]
             instances_sg.append(ec2_sg_id)
@@ -57,6 +62,18 @@ def report_not_used_security_groups(region):
         for sg in elbv2_sg:
             elbv2_sg_id = sg
             instances_sg.append(elbv2_sg_id) 
+            
+    if elasticcache_clusters != []: 
+        elastic_cache_sg = elasticcache_clusters[0]["CacheSecurityGroups"]
+        for sg in elastic_cache_sg:
+            elastic_cache_sg_id = sg
+            instances_sg.append(elastic_cache_sg_id) 
+            
+    if redshift_clusters != []: 
+        redshift_sg = redshift_clusters[0]["ClusterSecurityGroups"]
+        for sg in redshift_sg:
+            redshift_sg_id = sg
+            instances_sg.append(redshift_sg_id) 
     
     for sg_id in existing_security_groups.values():
         security_groups.append(sg_id)
@@ -69,16 +86,42 @@ def report_not_used_security_groups(region):
     used_sg_count = len(instances_sg)
     total_sg_count = unused_sg_count + used_sg_count
     
-    print(f"Total Security Groups: {total_sg_count}")
-    print(f"Used Security Groups: {used_sg_count}")
-    print(f"Un-Used Security Groups: {unused_sg_count} \n")
-    print(f"Security Groups (Not Used) ({ unused_sg_count }) - { security_groups }\n")
-    print(f"Security Groups (Used) ({ used_sg_count }) - { instances_sg }\n")
-    
-    return security_groups
-        
-report_not_used_security_groups('us-east-2')
+    return security_groups, total_sg_count, unused_sg_count, used_sg_count, instances_sg
 
+
+def delete_unused_security_groups(region='us-east-2'):
+    unused_security_groups = report_not_used_security_groups(region)[0]
+    my_config = Config(region_name=region)
+    ec2 = boto3.client('ec2', config=my_config)
+    for sg in unused_security_groups:
+        network = ec2.describe_network_interfaces(Filters=[{'Name': 'group-id', 'Values': [sg]}])["NetworkInterfaces"]
+        if network != []:
+            ec2_dependent = network[0]["Attachment"]["InstnaceId"]
+            print(f"Cannot delete {sg}. Has dependency to Instance: {ec2_dependent}")
+        else:
+            try:
+                ec2.delete_security_group(GroupId=sg)
+            except botocore.exceptions.ClientError as e:
+                print(f"Could not delete security group {sg}: {e}")
+                
+delete_unused_security_groups(region='us-east-1')
+
+# my_config = Config(region_name='us-east-1')
+# ec2 = boto3.client('ec2', config=my_config)
+# try:
+#     response = ec2.delete_security_group(GroupId='sg-0989599b843190361')
+#     print(response)
+# except botocore.exceptions.ClientError as e:
+#     print("Not allowed")
+    
+
+# sg_report = report_not_used_security_groups('us-east-2')
+
+# print(f"Total Security Groups: {sg_report[1]}")
+# print(f"Used Security Groups: {sg_report[3]}")
+# print(f"Un-Used Security Groups: {sg_report[2]} \n")
+# print(f"Security Groups (Not Used) ({sg_report[2]}) - {sg_report[0]}\n")
+# print(f"Security Groups (Used) ({sg_report[3]}) - {sg_report[4]}\n")
 
 
 
--- python/myscripts/testing.py
+++ python/myscripts/testing.py
@@ -1,44 +1,19 @@
 import boto3
+from botocore.config import Config
 
-if __name__ == "__main__":
-    ec2 = boto3.client("ec2")
-    elb = boto3.client("elb")
-    elbv2 = boto3.client("elbv2")
-    rds = boto3.client("rds")
+# Initialize a boto3 client
+my_config = Config(region_name = 'us-east-1')
+eb_client = boto3.client('elasticbeanstalk', config=my_config)
 
-    used_SG = set()
+# Specify your Elastic Beanstalk environment name
+environment_name = 'Vpro-app-11-9-env'
 
-    # Find EC2 instances security group in use.
-    response = ec2.describe_instances()
-    for reservation in response["Reservations"]:
-        for instance in reservation["Instances"]:
-            for sg in instance["SecurityGroups"]:
-                used_SG.add(sg["GroupId"])
+# Retrieve the environment description
+response = eb_client.describe_environment_resources(EnvironmentName=environment_name)
 
-    # Find Classic load balancer security group in use
-    response = elb.describe_load_balancers()
-    for lb in response["LoadBalancerDescriptions"]:
-        for sg in lb["SecurityGroups"]:
-            used_SG.add(sg)
-
-    # Find Application load balancer security group in use
-    response = elbv2.describe_load_balancers()
-    for lb in response["LoadBalancers"]:
-        for sg in lb["SecurityGroups"]:
-            used_SG.add(sg)
-            
-
-    # Find RDS db security group in use
-    response = rds.describe_db_instances()
-    for instance in response["DBInstances"]:
-        for sg in instance["VpcSecurityGroups"]:
-            used_SG.add(sg["VpcSecurityGroupId"])
-
-    response = ec2.describe_security_groups()
-    total_SG = [sg["GroupId"] for sg in response["SecurityGroups"]]
-    unused_SG = set(total_SG) - used_SG
-
-    print(f"Total Security Groups: {len(total_SG)}")
-    print(f"Used Security Groups: {len(used_SG)}\n")
-    print(f"Unused Security Groups: {len(unused_SG)} compiled in the following list:")
-    print(f"{list(unused_SG)}")
+# Navigate through the response to find the security groups
+instances = response['EnvironmentResources']['Instances']
+for instance in instances:
+    print(f"Instance ID: {instance['Id']}")
+    for security_group in instance['SecurityGroups']:
+        print(f"Security Group ID: {security_group}")
