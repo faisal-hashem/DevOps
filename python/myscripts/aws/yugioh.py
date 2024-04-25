1a6bb94c704aade67c9e4274a4b44b781ce0b0c6
Faisal Hashem <faisal-hashem@outlook.com>
2024-04-25 00:10:44 -0400

    Updated

--- python/myscripts/aws/yugioh.py
+++ python/myscripts/aws/yugioh.py
@@ -19,7 +19,6 @@ def draw_cards(data):
     return hand
 
 def display_card(hand):
-    print("\n")
     for card in hand:
         if "Monster" in card["type"]:
             print(f"{card["name"]} - Type: {card['type']} - Attack: {card["atk"]} - Defense: {card['def']} - Level: {card['level']} - Race: {card['race']}\n")
@@ -27,10 +26,16 @@ def display_card(hand):
             print(f"{card["name"]} - Type: {card['type']} - Description: {card['desc']}\n")   
 
 def main():
+    print("Welcome to YuGiOh!\n")
+    print("Lets draw some cards!\n")
+    print("Here is your hand.\n")
     url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
     data = retrieve_data(url)
-    hand = draw_cards(data)
-    display_card(hand)
+    trap = display_card(data)
+    # your_hand = draw_cards(data)
+    # display_card(your_hand)
+    # computer_hand = draw_cards(data)
+    
     
 if __name__ == "__main__":
     main()
